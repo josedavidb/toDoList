500c42bd23e0fb06e2d5a48f3b73ba1fe1f90029
Jose Basanta <josedavidb6@gmail.com>
2019-04-19 13:34:50 -0400

    Created models

--- toDo/models.py
+++ toDo/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class TodoList(models.Model):
+
+    title = models.CharField(max_length=250) 
+    content = models.TextField(blank=True)
+    created = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
+    due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
