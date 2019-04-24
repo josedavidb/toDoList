ba0949cf3ccf3d254bedf869ec0314e57b0597a6
Jose Basanta <josedavidb6@gmail.com>
2019-04-24 15:26:15 -0400

    Fixed bug to deploy

--- toDoList/settings.py
+++ toDoList/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5skl5@ju@lf%=x=05chb77j5pkv8zv=)jhc4x!sz7ltjnt-k+)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
